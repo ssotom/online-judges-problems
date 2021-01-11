7eaac0772300f720964dfd5af183e090fc60fe6f
Santiago Soto <santiago.soto@talosdigital.com>
2021-01-10 20:26:07 -0500

    Code refactoring

--- Uva/TheBusDriverProblem.java
+++ Uva/TheBusDriverProblem.java
@@ -9,34 +9,39 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class TheBusDriverProblem{
-    public static void main(String args[]){
-        int numDrivers, duration, extra;
-        int[] morning, evening;
-        
-        Scanner sc = new Scanner(System.in);
-    
-        while(true){
-            numDrivers = sc.nextInt();
-            duration = sc.nextInt();
-            extra = sc.nextInt();
-            if(numDrivers == 0 && duration == 0 && extra == 0)   break;
-        
-            morning = new int[numDrivers];
-            evening = new int[numDrivers];
-        
-            for(int i = 0; i < numDrivers; i++)  morning[i] = sc.nextInt();
-            for(int i = 0; i < numDrivers; i++)  evening[i] = -sc.nextInt();
-        
-            Arrays.sort(morning); Arrays.sort(evening);
-        
-            int val = 0;
-            for(int i = 0; i < numDrivers; i++){
-                int result = morning[i] - (evening[i]);
-                if(result > duration)
-                    val += (result - duration) * extra;
-            }
-            System.out.println(val);
-        }
-    }
+
+public class TheBusDriverProblem {
+	public static void main(String[] args) {
+		int numDrivers, duration, extra;
+		int[] morning, evening;
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			numDrivers = sc.nextInt();
+			duration = sc.nextInt();
+			extra = sc.nextInt();
+
+			if (numDrivers == 0 && duration == 0 && extra == 0) break;
+
+			morning = new int[numDrivers];
+			evening = new int[numDrivers];
+
+			for (int i = 0; i < numDrivers; i++)
+				morning[i] = sc.nextInt();
+			for (int i = 0; i < numDrivers; i++)
+				evening[i] = -sc.nextInt();
+
+			Arrays.sort(morning);
+			Arrays.sort(evening);
+
+			int val = 0;
+			for (int i = 0; i < numDrivers; i++) {
+				int result = morning[i] - (evening[i]);
+				if (result > duration)
+					val += (result - duration) * extra;
+			}
+			System.out.println(val);
+		}
+	}
 }
--- Uva/TrainSwapping.java
+++ Uva/TrainSwapping.java
@@ -8,28 +8,32 @@
 
 import java.util.Scanner;
 
-public class TrainSwapping{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in); 
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            int temp;
-            int count = 0;
-            int l = sc.nextInt();
-            int order[] = new int[l];
-            for(int j = 0; j < l; j++)
-                order[j] = sc.nextInt();
-            for(int j = 0; j < l; j++){
-              for(int k = 0; k < l-1; k++){
-                  if(order[k] > order[k+1]){
-                      temp = order[k+1];
-                      order[k+1] = order[k];
-                      order[k] = temp;
-                      count++;
-                  }
-              }
-            }
-            System.out.println("Optimal train swapping takes " + count + " swaps.");
-        }
-    }
+
+public class TrainSwapping {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int temp;
+			int count = 0;
+			int l = sc.nextInt();
+			int[] order = new int[l];
+
+			for (int j = 0; j < l; j++)
+				order[j] = sc.nextInt();
+
+			for (int j = 0; j < l; j++) {
+				for (int k = 0; k < l - 1; k++) {
+					if (order[k] > order[k + 1]) {
+						temp = order[k + 1];
+						order[k + 1] = order[k];
+						order[k] = temp;
+						count++;
+					}
+				}
+			}
+			System.out.println("Optimal train swapping takes " + count + " swaps.");
+		}
+	}
 }
